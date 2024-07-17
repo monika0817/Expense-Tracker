578e6156150cfc02851c0332206b32e93dce5353
monika0817 <monikamonika0117@gmail.com>
2024-07-17 18:17:04 +0530

    update commit

--- app/(routes)/dashboard/budgets/page.jsx
+++ app/(routes)/dashboard/budgets/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import BudgetList from './_components/BudgetList'
+import BudgetList from './_components/BudgetList';
 function Budgets() {
   return (
     <div className='p-10'>
